ffeb4fd23960ecfefebc0d5a3fb593296582775f
lavanya sehgal <lavanyasehgal3008@gmail.com>
2026-09-03 09:39:16 +0530

    pq

--- priorityQueueAscending.cpp
+++ priorityQueueAscending.cpp
@@ -1,56 +1,18 @@
 #include<iostream>
+#include<queue>
 using namespace std;
-class PriorityQueue {
 
-
-    int arr[100]; 
-    
-    int size;
-
-    public:
-        PriorityQueue(){
-            size=0;
-        }
-        void enqueue(int data){
-            int i=size-1;
-            while(i>=0 && arr[i]>data){
-            i--;
-        }
-            arr[i+1]=data;
-            size++;
-        }
-        void dequeue(){
-            if(size==0){
-                cout<<"Queue is empty"<<endl;
-                return;
-            }
-            for(int i=0;i<size-1;i++){
-                arr[i]=arr[i+1];
-            }
-            size--;
-        }
-        void display(){
-            if(size==0){
-                cout<<"priority Queue is empty\n";
-                return;
-            }
-            for(int i=0;i<size;i++){
-                cout<<arr[i]<<" ";
-            }
-            cout<<endl;
-        }
-    };
-    int main(){
-        PriorityQueue pq;
-
-        pq.enqueue(10);
-        pq.enqueue(20);
-        pq.enqueue(30);
-        pq.enqueue(5);
-        pq.display();
-        pq.dequeue();
-        pq.display();
-    
-    
-    return 0;
+int main(){
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(30);
+    pq.push(10);
+    pq.push(50);
+    pq.push(20);
+    pq.push(40);
+    cout<<"Elements in priority queue: ";
+    while(!pq.empty()){
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+    cout<<endl;
 }
