397a002bd2d89e6810c88dd7202d940e7df9713d
lavanya sehgal <lavanyasehgal3008@gmail.com>
2026-09-11 15:30:08 +0530

    changes in mergesort

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,37 +1,29 @@
 #include<iostream>
 using namespace std;
-void merge(int a[],int low,int mid,int high){
-    int i,j,k;
+
+void merge(int a[], int low, int mid, int high) {
+    int i = low, j = mid + 1, k = low;
     int c[10];
-    i=low;
-    j=mid+1;
-    k=low;
-    while(i<=mid && j<=high){
-        if(a[i]<a[j]){
-            c[k]=a[i];
-            i++;
-            k++;
+
+    while (i <= mid && j <= high) {
+        if (a[i] < a[j]) {
+            c[k++] = a[i++];
+        } else {
+            c[k++] = a[j++];
         }
-        else{
-            c[k]=a[j];
-            j++;
-            k++;
-        }
-    }
-    while(i<=mid){
-        c[k]=a[i];
-        j++;
-        k++;
-    }
-    while(j<=high){
-        c[k]=a[j];
-        j++;
-        k++;
-    }
-    for(i=low;i<=high;i++){
-        a[i]=c[i];
     }
 
+    while (i <= mid) {
+        c[k++] = a[i++];
+    }
+
+    while (j <= high) {
+        c[k++] = a[j++];
+    }
+
+    for (i = low; i <= high; i++) {
+        a[i] = c[i];
+    }
 }
 void mergesort(int a[],int low,int high){
     if(low<high){
